a3afca57995ba1c23cb1a530b6fc8de418debc33
Smit Gadhavi <94707089+smit1271@users.noreply.github.com>
2023-07-19 11:39:04 +0530

    Add files via upload

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../Components/Banner/Banner"
+import CoinsTable from "../Components/CoinTable";
+
+const Homepage = () => {
+    return (
+        <>
+            <Banner />
+            <CoinsTable />
+        </>
+    );
+};
+
+export default Homepage;
